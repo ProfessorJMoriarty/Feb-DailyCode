713cf8940b39c0df99ef92299e6f79b65ff5fc19
Game110AM <Game110AM@605-110-STU11>
2017-02-02 07:57:32 -0700

    yes

---
+++ Feb2/Feb2/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char input;
+	cout << "What song would you love to listen to?" << endl;
+	cin>>input;
+	if (input == '1')
+		cout << "How can this happen to me?" << endl;
+	if(input == '2')
+		cout << "Wake me up inside!" << endl;
+	if (input == '3')
+		cout << "Wake me up when its all over" << endl;
+
+
+
+}
