b41ed44d7daeb4bea0b0483a7ece1e1217864607
Game110AM <Game110AM@605-110-STU11>
2017-02-07 07:53:31 -0700

    yes

---
+++ Feb7/Feb7/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int input;
+	cin >> input;
+	if (input % 2 == 0)
+	cout << "Is your name even steven?" << endl;
+	if (input % 2 == 1)
+		cout << "You are an odd ball!"<<endl;
+
+}
