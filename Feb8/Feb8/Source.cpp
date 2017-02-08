485ae72b3c21d86960925b7167b24caf891eb200
Game110AM <Game110AM@605-110-STU11>
2017-02-08 08:06:28 -0700

    yes

---
+++ Feb8/Feb8/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int input1;
+	int input2;
+		cout << "What is the height of the cone? " << endl;
+	cin >> input1;
+	cout << "What is the length of the cone?" << endl;
+	cin >> input2;
+	cout << "The volume of the cone is" << input1*input2 / 3 << "!!!"<<endl;
+
+
+}
