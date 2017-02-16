eef4c5b864818a1f9404aa64102d7f2da9f8b6e4
Game110AM <Game110AM@605-110-STU11>
2017-02-16 09:18:18 -0700

    yes

--- Feb16/Feb16/Source.cpp
+++ Feb16/Feb16/Source.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
 	for (int i = 4; i < 27; i = i + 3)
 		cout << i << endl;
-	for (int i = 2; i < 64; i = i + 7)
+	for (int i = 2; i < 10; i = i + 7)
 		cout << i << endl;
-	for (int i = 25; i < 100; i = i * 6)
+	for (int i = 10; i < 15; i = i+2)
 		cout << i << endl;
 }
--- Princess Quiz/Princess Quiz/Source.cpp
+++ Princess Quiz/Princess Quiz/Source.cpp
@@ -13,133 +13,133 @@ int main() {
 	cout << "What is your favorite basketball shot? (Pnr=P Dunk=D ThreePoint=T Fadeaway=F)" << endl;
 	cin >> input;
 
-	if (input == 'P');  
+	if (input == 'P') 
 	 Paul = Paul + 1;
-	 if (input == 'D');
+	else if (input == 'D')
 	 Lebron = Lebron + 1;
-	if (input == 'T');
+	else if (input == 'T')
 	 Curry = Curry + 1;
-	if (input == 'F');
+	else 
 	 Mamba = Mamba + 1;
 
 
 	cout << "What is your favorite basketball Position? (PointGuard=P ShootingGuard=S SmallForward=S Center=C)" << endl;
 	cin >> input;
 
-	if (input == 'P');
+	if (input == 'P')
 	Paul = Paul + 1;
-	if (input == 'S');
+	else if (input == 'S')
 	Lebron = Lebron + 1;
-	if (input == 'C');
+	else if (input == 'C')
 	Curry = Curry + 1;
-	if (input == 'S');
+	else 
 	Mamba = Mamba + 1;
 
 
 	cout << "What is your favorite basketball Player? (Magic Johnson=P Michael Jordan=M  Dr J=D Reggie Miller=R)" << endl;
 	cin >> input;
 
-	if (input == 'P');
+	if (input == 'P')
 	Paul = Paul + 1;
-	if (input == 'D');
+	else if (input == 'D')
 	Lebron = Lebron + 1;
-	if (input == 'R');
+	else if (input == 'R')
 	Curry = Curry + 1;
-	if (input == 'M');
+	else 
 	Mamba = Mamba + 1;
 
 	cout << "What is your favorite basketball coach? (Doc Rivers=D Phil Jackson=P  Eric Spoelstra=E Steve Kerr=S)" << endl;
 	cin >> input;
 
-	if (input == 'D');
+	if (input == 'D')
 	Paul = Paul + 1;
-	if (input == 'E');
+	else if (input == 'E')
 	Lebron = Lebron + 1;
-	if (input == 'S');
+	else if (input == 'S')
 	Curry = Curry + 1;
-	if (input == 'P');
+	else 
 	Mamba = Mamba + 1;
 
 
 	cout << "It's the final play of the game what do you do? (Off Dribble Three=O PicknRoll=R  Drive and Dunk=D Draw a triple team and shot a fadeaway=G)" << endl;
 	cin >> input;
 
-	if (input == 'D');
+	if (input == 'D')
 	Paul = Paul + 1;
-	if (input == 'E');
+	else if (input == 'E')
 	Lebron = Lebron + 1;
-	if (input == 'S');
+	else if (input == 'S')
 	Curry = Curry + 1;
-	if (input == 'P');
+	else 
 	Mamba = Mamba + 1;
 	
 	cout << "What feature best describes you? (Intelligent=I Loyal=L  Determined=D Humour=G)" << endl;
 	cin >> input;
 
-	if (input == 'I');
+	if (input == 'I')
 	Paul = Paul + 1;
-	if (input == 'L');
+	else if (input == 'L')
 	Lebron = Lebron + 1;
-	if (input == 'G');
+	else if (input == 'G')
 	Curry = Curry + 1;
-	if (input == 'D');
+	else 
 	Mamba = Mamba + 1;
 
 	cout << "What is your best basketball trait?? (Assits=A Ball Handling=B  Shooting=S Basketball IQ=G)" << endl;
 	cin >> input;
 
-	if (input == 'A');
+	if (input == 'A')
 	Paul = Paul + 1;
-	if (input == 'G');
+	else if (input == 'G')
 	Lebron = Lebron + 1;
-	if (input == 'B');
+	else if (input == 'B')
 	Curry = Curry + 1;
-	if (input == 'S');
+	else 
 	Mamba = Mamba + 1;
 
 	cout << "When you're playing defense, what do you do? (James Harden=J Use your physical adavantages to keep out of the lane=P  Use small size to easily steal the ball=Q  Be propane, be in his grill all day=R)" << endl;
 	cin >> input;
 
-	if (input == 'Q');
+	if (input == 'Q')
 	Paul = Paul + 1;
-	if (input == 'P');
+	else if (input == 'P')
 	Lebron = Lebron + 1;
-	if (input == 'J');
-	Curry = Curry + 1;
-	if (input == 'R');
+	else if (input == 'J')
+	Curry = Curry + 1; 
+	else 
 	Mamba = Mamba + 1;
 
 	cout << "What team do you prefer? (Lakers=L Clippers=C  Warriors=W  Heat=H)" << endl;
 	cin >> input;
 
-	if (input == 'C');
+	if (input == 'C')
 	Paul = Paul + 1;
-	if (input == 'H');
+	else if (input == 'H')
 	Lebron = Lebron + 1;
-	if (input == 'W');
+	else if (input == 'W')
 	Curry = Curry + 1;
-	if (input == 'L');
+	else 
 	Mamba = Mamba + 1;
 
 	cout << "What number do you like most? (Three=3 Six=6  Thirty=30  Twenty Three=23)" << endl;
 	cin >> input;
 
-	if (input == '3');
+	if (input == '3')
 	Paul = Paul + 1;
-	if (input == '6');
+	else if (input == '6')
 	Lebron = Lebron + 1;
-	if (input == '30');
+	else if (input == '30')
 	Curry = Curry + 1;
-	if  (input == '23');
+	else
 	Mamba = Mamba + 1;
 
-	if ((Paul > Lebron) && (Paul > Curry) && (Paul > Mamba));
+	if ((Paul > Lebron) && (Paul > Curry) && (Paul > Mamba))
 	cout << "YOU ARE PAUL" << endl;
-	 if  ((Lebron > Paul) && (Lebron > Curry) && (Lebron > Mamba));
+	 else if  ((Lebron > Paul) && (Lebron > Curry) && (Lebron > Mamba))
 	cout << "YOU ARE LEBRON" << endl;
-	if ((Curry > Lebron) && (Curry > Paul) && (Curry > Mamba));
+	else if ((Curry > Lebron) && (Curry > Paul) && (Curry > Mamba))
 	cout << "YOU ARE CURRY" << endl;
-	if ((Mamba > Lebron) && (Mamba > Curry) && (Mamba > Paul));
+	else if ((Mamba > Lebron) && (Mamba > Curry) && (Mamba > Paul))
 	cout << "YOU ARE THE BLACK MAMBA" << endl;
 
 }
