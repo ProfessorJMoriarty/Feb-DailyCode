43be7726acde453bfe56c069db290b5d5e6227b1
Game110AM <Game110AM@605-110-STU11>
2017-02-17 07:50:00 -0700

    yes

---
+++ Feb17/Feb17/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char input ='y';
+	while (input == 'y') {
+		cout << "Would you like a cookie you sexy beast" << endl;
+		cin >> input;
+	}
+
+}
